5171b5a70cde318c1835a69eec5f9dd4d967f283
GofmanDaniil <Tyuiu.GofmanDV@outlook.com>
2023-10-22 22:25:31 +0500

    Add project files.

---
+++ Tyuiu.GofmanDV.Sprint3.Task0.V19.Lib/DataService.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using tyuiu.cources.programming.interfaces.Sprint3;
+namespace Tyuiu.GofmanDV.Sprint3.Task0.V19.Lib
+{
+    public class DataService : ISprint3Task0V19
+    {
+        public double GetSumSeries(int startValue, int stopValue)
+        {
+            double sumSeries = 0;
+            int k;
+            for (k = startValue; k <= stopValue; k++)
+            {
+                sumSeries = Math.Pow((1/(Math.Cos(k) + 2)), 2);
+            }
+            return Math.Round(sumSeries, 3);
+        }
+    }
+}
---
+++ Tyuiu.GofmanDV.Sprint3.Task0.V19.Test/DataServiceTest.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.GofmanDV.Sprint3.Task0.V19.Lib;
+
+namespace Tyuiu.GofmanDV.Sprint3.Task0.V19.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidGetSumSeries()
+        {
+            DataService ds = new DataService();
+            int k1 = 1, k2 = 10;
+
+            var sum = ds.GetSumSeries(k1, k2);
+            double z = 0.742;
+
+
+            Assert.AreEqual(z, sum);
+        }
+    }
+}
