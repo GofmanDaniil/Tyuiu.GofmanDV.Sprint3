c6eb4207e74977b8d752c2cf867dc0d46617cfd0
GofmanDaniil <Tyuiu.GofmanDV@outlook.com>
2023-10-22 22:50:04 +0500

    Task 0

---
+++ Tyuiu.GofmanDV.Sprint3.Task2.V7.Test/DataServiceTest.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.GofmanDV.Sprint3.Task2.V7.Lib;
+
+namespace Tyuiu.GofmanDV.Sprint3.Task2.V7.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidGetSumSeries()
+        {
+            DataService ds = new DataService();
+            int startValue = 1;
+            int stopValue = 10;
+
+            double res = ds.GetSumSeries(startValue, stopValue);
+            double wait = 98220.47;
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
--- Tyuiu.GofmanDV.Sprint3.Task2.V7/Program.cs
+++ Tyuiu.GofmanDV.Sprint3.Task2.V7/Program.cs
@@ -3,39 +3,45 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Tyuiu.GofmanDV.Sprint3.Task0.V19.Lib;
+using Tyuiu.GofmanDV.Sprint3.Task2.V7.Lib;
 
-namespace Tyuiu.GofmanDV.Sprint3.Task0.V19
+namespace Tyuiu.GofmanDV.Sprint3.Task2.V7
 {
-    internal class Program
+    class Program
     {
         static void Main(string[] args)
         {
             DataService ds = new DataService();
+
             Console.Title = "Спринт #3 | Выполнил: Гофман Д. В. | СМАРТб-23-1";
+
             Console.WriteLine("***************************************************************************");
             Console.WriteLine("* Спринт #3                                                               *");
-            Console.WriteLine("* Тема: Оператор цикла for                                                *");
-            Console.WriteLine("* Задание #0                                                              *");
-            Console.WriteLine("* Вариант #19                                                             *");
-            Console.WriteLine("* Выполнил: Гофман Даниил Викторович | СМАРТб-23-1                        *");
+            Console.WriteLine("* Тема: Оператор цикла do-while                                           *");
+            Console.WriteLine("* Задание #2                                                              *");
+            Console.WriteLine("* Вариант #7                                                              *");
+            Console.WriteLine("* Выполнил: Гофман Даниил Викторович | Смартб-23-1                        *");
             Console.WriteLine("***************************************************************************");
             Console.WriteLine("* УСЛОВИЕ:                                                                *");
-            Console.WriteLine("* Написать программу используя цикл for, которая вычисляет сумму ряда по  *");
-            Console.WriteLine("* формуле.                                                                *");
+            Console.WriteLine("* Написать программу используя цикл do...while,                           *");
+            Console.WriteLine("* которая вычисляет произведение ряда по формуле.                         *");
             Console.WriteLine("*                                                                         *");
             Console.WriteLine("***************************************************************************");
             Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
             Console.WriteLine("***************************************************************************");
-            int startValue = 1, stopValue = 10;
-            Console.WriteLine("Старт шага = " + startValue);
-            Console.WriteLine("Конец шага = " + stopValue);
 
+            int startValue = 1;
+            int stopValue = 10;
+
+            Console.WriteLine("Старт шага: " + startValue);
+            Console.WriteLine("Конец шага: " + stopValue);
 
             Console.WriteLine("***************************************************************************");
             Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
             Console.WriteLine("***************************************************************************");
-            Console.WriteLine("Сумма ряда " + ds.GetSumSeries(startValue, stopValue));
+
+            Console.WriteLine("Сумма ряда = " + ds.GetSumSeries(startValue, stopValue));
+
             Console.ReadKey();
         }
     }
