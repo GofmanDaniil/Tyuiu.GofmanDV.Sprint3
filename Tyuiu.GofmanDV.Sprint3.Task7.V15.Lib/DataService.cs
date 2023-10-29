3d09695ce9c36339c884393ecbe8289f79ebd2fd
GofmanDaniil <Tyuiu.GofmanDV@outlook.com>
2023-10-29 21:35:35 +0500

    Task7

---
+++ Tyuiu.GofmanDV.Sprint3.Task7.V15.Lib/DataService.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using tyuiu.cources.programming.interfaces.Sprint3;
+
+namespace Tyuiu.GofmanDV.Sprint3.Task7.V15.Lib
+{
+    public class DataService : ISprint3Task7V15
+    {
+        public double[] GetMassFunction(int startValue, int stopValue)
+        {
+            double[] valueArray;
+            int len = (stopValue - startValue) + 1;
+            valueArray = new double[len];
+            double y;
+            double y1;
+            int count = 0;
+            for (int x = startValue; x <= stopValue; x++)
+            {
+                y1 = (Math.Sin(x) + ((2 * x) / 3));
+                y = Math.Round(y1 - (Math.Cos(x) * 4 * x), 2);
+                valueArray[count] = Math.Round(y, 2);
+                count++;
+            }
+            return valueArray;
+        }
+    }
+}
---
+++ Tyuiu.GofmanDV.Sprint3.Task7.V15.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.GofmanDV.Sprint3.Task7.V15.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.GofmanDV.Sprint3.Task7.V15.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("6b1a182d-61f4-4b05-9a8a-5d9a2d5810fd")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
