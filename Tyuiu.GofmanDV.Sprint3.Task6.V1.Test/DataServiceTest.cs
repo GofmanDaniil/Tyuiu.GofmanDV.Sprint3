2fadb0ee8b5422b2f25393502b883baf64b0e400
GofmanDaniil <Tyuiu.GofmanDV@outlook.com>
2023-10-29 21:25:05 +0500

    Task6

---
+++ Tyuiu.GofmanDV.Sprint3.Task6.V1.Test/DataServiceTest.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.GofmanDV.Sprint3.Task6.V1.Lib;
+
+namespace Tyuiu.GofmanDV.Sprint3.Task6.V1.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            Assert.AreEqual(151, ds.GetSumTheDivisors(11, 17));
+        }
+    }
+}
