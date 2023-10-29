d366a5ba7afd7bf68a649e8542cdd11953be0bad
GofmanDaniil <Tyuiu.GofmanDV@outlook.com>
2023-10-29 21:04:04 +0500

    Task5

---
+++ Tyuiu.GofmanDV.Sprint3.Task5.V7.Lib/DataService.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using tyuiu.cources.programming.interfaces.Sprint3;
+
+namespace Tyuiu.GofmanDV.Sprint3.Task5.V7.Lib
+{
+    public class DataService : ISprint3Task5V7
+    {
+        public double GetSumSumSeries(int startValue1, int startValue2, int stopValue1, int stopValue2, int x)
+        {
+            double SumSeries = 0;
+            int i, j;
+            for (i = startValue1; i <= stopValue1; i++)
+            {
+                for (j = startValue2; j <= stopValue2; j++)
+                {
+                    SumSeries += 1 / Math.Cos(j) + x;
+                }
+            }
+            return Math.Round(SumSeries, 3);
+        }
+    }
+}
---
+++ Tyuiu.GofmanDV.Sprint3.Task5.V7.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.GofmanDV.Sprint3.Task5.V7.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.GofmanDV.Sprint3.Task5.V7.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("a1f9a606-c7f5-4585-817f-6e251bac77be")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
